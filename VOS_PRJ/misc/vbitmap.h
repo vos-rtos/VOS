7e2b0dd6d2c8e4734d1ba053d0b3d7ed32fe6ac4
vincent <vincent@gmail.com>
2020-08-20 12:35:33 +0800

    1 把位图操作归纳到单个文件里。

---
+++ VOS_PRJ/misc/vbitmap.h
@@ -0,0 +1,13 @@
+#ifndef __V_BITMAP_H__
+#define __V_BITMAP_H__
+/*
+  * 位图操作宏定义
+ */
+#define bitmap_for_each(pos, bitmap_byte) \
+	for (pos = 0; pos < bitmap_byte*8; pos++)
+
+#define bitmap_get(n, bitmap)		(!!(((u8*)(bitmap))[(n)>>3] & 1<<((n)&0x07)))
+#define bitmap_clear(n, bitmap)		(((u8*)(bitmap))[(n)>>3] &= ~(1<<((n)&0x07)))
+#define bitmap_set(n, bitmap)		(((u8*)(bitmap))[(n)>>3] |= 1<<((n)&0x07))
+
+#endif
--- VOS_PRJ/vos/vos.h
+++ VOS_PRJ/vos/vos.h
@@ -16,15 +16,7 @@
 
 typedef void (*task_fun_t)(void *param);
 
-/*
-  * 位图操作宏定义
- */
-#define bitmap_for_each(pos, bitmap_byte) \
-	for (pos = 0; pos < bitmap_byte*8; pos++)
 
-#define bitmap_get(n, bitmap)		(!!(((u8*)(bitmap))[(n)>>3] & 1<<((n)&0x07)))
-#define bitmap_clear(n, bitmap)		(((u8*)(bitmap))[(n)>>3] &= ~(1<<((n)&0x07)))
-#define bitmap_set(n, bitmap)		(((u8*)(bitmap))[(n)>>3] |= 1<<((n)&0x07))
 
 
 
