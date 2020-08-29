e19319002f44c3f4e8140a51d603a0f438465d88
vincent <vincent@gmail.com>
2020-08-29 10:13:04 +0800

    1 整理代码。

--- VOS_PRJ/usart/usart.c
+++ VOS_PRJ/usart/usart.c
@@ -1,16 +1,20 @@
+/********************************************************************************************************
+* 版    权: Copyright (c) 2020, VOS Open source. All rights reserved.
+* 文    件: sart.c
+* 作    者: 156439848@qq.com; vincent_cws2008@gmail.com
+* 版    本: VOS V1.0
+* 历    史：
+* --20200801：创建文件
+* --20200828：添加注释
+*********************************************************************************************************/
+
 #include "usart.h"
 #include "vos.h"
 #include "vringbuf.h"
 
 #define USART_RX_USE_DMA	1
 
-int fputc(int ch, FILE *f)
-{ 	
-	USART1->SR = (uint16_t)~0x0040; //解决第一个字节发不出来问题
-	while((USART1->SR&0X40)==0);
-	USART1->DR = (u8) ch;      
-	return ch;
-}
+
 #define SEND_BUF_SIZE 512
 #define RECV_BUF_SIZE 512
 u8 SendBuff[SEND_BUF_SIZE];
@@ -31,13 +35,33 @@ typedef struct StMapEvtTask{
 }StMapEvtTask;
 
 StMapEvtTask gArrUart[1];
+
+
+/********************************************************************************************************
+* 函数：void RegistUartEvent(s32 event, s32 task_id);
+* 描述:   注册串口事件，vshell需要这个事件来处理用户输入命令
+* 参数:
+* [1] event: 事件号
+* [2] task_id: 任务ID
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 void RegistUartEvent(s32 event, s32 task_id)
 {
 	gArrUart[0].event = event;
 	gArrUart[0].task_id = task_id;
 }
 
-void uart_init(u32 bound){
+/********************************************************************************************************
+* 函数：void uart_init(u32 bound);
+* 描述:   USART1 初始化
+* 参数:
+* [1] bound: 波特率
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
+void uart_init(u32 bound)
+{
 
 	GPIO_InitTypeDef GPIO_InitStructure;
 	USART_InitTypeDef USART_InitStructure;
@@ -85,110 +109,119 @@ void uart_init(u32 bound){
  	gRingBuf = VOSRingBufFormat(gRxRingBuf, sizeof(gRxRingBuf));
 }
 
+/********************************************************************************************************
+* 函数： void DMA_USART1_Tx_Init(u8 *dma_buf, s32 len);
+* 描述:   USART1 DMA发送初始化
+* 参数:
+* [1] dma_buf: dma buffer地址
+* [2] len: dma buffer 长度
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 void DMA_USART1_Tx_Init(u8 *dma_buf, s32 len)
 {
     NVIC_InitTypeDef NVIC_InitStructure;
     DMA_InitTypeDef DMA_InitStructure;
 
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE); // 使能DMA2时钟
 
-    /* 1.使能DMA2时钟 */
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
+    DMA_DeInit(DMA2_Stream7); // 配置使用DMA发送数据
 
-    /* 2.配置使用DMA发送数据 */
-    DMA_DeInit(DMA2_Stream7);
-
-    DMA_InitStructure.DMA_Channel             = DMA_Channel_4;               /* 配置DMA通道 */
-    DMA_InitStructure.DMA_PeripheralBaseAddr  = (uint32_t)(&(USART1->DR));   /* 目的 */
-    DMA_InitStructure.DMA_Memory0BaseAddr     = (uint32_t)dma_buf;             /* 源 */
-    DMA_InitStructure.DMA_DIR                 = DMA_DIR_MemoryToPeripheral;    /* 方向 */
-    DMA_InitStructure.DMA_BufferSize          = len;                    		/* 长度 */
-    DMA_InitStructure.DMA_PeripheralInc       = DMA_PeripheralInc_Disable;    /* 外设地址是否自增 */
-    DMA_InitStructure.DMA_MemoryInc           = DMA_MemoryInc_Enable;         /* 内存地址是否自增 */
-    DMA_InitStructure.DMA_PeripheralDataSize  = DMA_MemoryDataSize_Byte;      /* 目的数据带宽 */
-    DMA_InitStructure.DMA_MemoryDataSize      = DMA_MemoryDataSize_Byte;      /* 源数据宽度 */
-    DMA_InitStructure.DMA_Mode                = DMA_Mode_Normal;              /* 单次传输模式/循环传输模式 */
-    DMA_InitStructure.DMA_Priority            = DMA_Priority_High;             /* DMA优先级 */
-    DMA_InitStructure.DMA_FIFOMode            = DMA_FIFOMode_Disable;          /* FIFO模式/直接模式 */
-    DMA_InitStructure.DMA_FIFOThreshold       = DMA_FIFOThreshold_HalfFull; /* FIFO大小 */
-    DMA_InitStructure.DMA_MemoryBurst         = DMA_MemoryBurst_Single;       /* 单次传输 */
+    DMA_InitStructure.DMA_Channel             = DMA_Channel_4;               	// 配置DMA通道
+    DMA_InitStructure.DMA_PeripheralBaseAddr  = (uint32_t)(&(USART1->DR));   	// 目的
+    DMA_InitStructure.DMA_Memory0BaseAddr     = (uint32_t)dma_buf;             	// 源
+    DMA_InitStructure.DMA_DIR                 = DMA_DIR_MemoryToPeripheral;    	// 方向
+    DMA_InitStructure.DMA_BufferSize          = len;                    		// 长度
+    DMA_InitStructure.DMA_PeripheralInc       = DMA_PeripheralInc_Disable;    	// 外设地址是否自增
+    DMA_InitStructure.DMA_MemoryInc           = DMA_MemoryInc_Enable;         	// 内存地址是否自增
+    DMA_InitStructure.DMA_PeripheralDataSize  = DMA_MemoryDataSize_Byte;      	// 目的数据带宽
+    DMA_InitStructure.DMA_MemoryDataSize      = DMA_MemoryDataSize_Byte;      	// 源数据宽度
+    DMA_InitStructure.DMA_Mode                = DMA_Mode_Normal;              	// 单次传输模式/循环传输模式
+    DMA_InitStructure.DMA_Priority            = DMA_Priority_High;             	// DMA优先级
+    DMA_InitStructure.DMA_FIFOMode            = DMA_FIFOMode_Disable;          	// FIFO模式/直接模式
+    DMA_InitStructure.DMA_FIFOThreshold       = DMA_FIFOThreshold_HalfFull; 	// FIFO大小
+    DMA_InitStructure.DMA_MemoryBurst         = DMA_MemoryBurst_Single;       	// 单次传输
     DMA_InitStructure.DMA_PeripheralBurst     = DMA_PeripheralBurst_Single;
 
-    /* 3. 配置DMA */
-    DMA_Init(DMA2_Stream7, &DMA_InitStructure);
+    DMA_Init(DMA2_Stream7, &DMA_InitStructure); 	// 配置DMA
+    DMA_ITConfig(DMA2_Stream7, DMA_IT_TC, ENABLE); 	// 使能DMA中断
+    USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);  // 使能串口的DMA发送接口
 
-    /* 4.使能DMA中断 */
-    DMA_ITConfig(DMA2_Stream7, DMA_IT_TC, ENABLE);
-
-    /* 5.使能串口的DMA发送接口 */
-    USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
-
-    /* 6. 配置DMA中断优先级 */
+    // 配置DMA中断优先级
     NVIC_InitStructure.NVIC_IRQChannel                   = DMA2_Stream7_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 3;
     NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 
-    /* 7.不使能DMA */
+    // 不使能DMA
     DMA_Cmd(DMA2_Stream7, DISABLE);
 }
 
+/********************************************************************************************************
+* 函数： void DMA_USART1_Rx_Init(u8 *dma_buf, s32 len);
+* 描述:   USART1 DMA接收初始化
+* 参数:
+* [1] dma_buf: dma buffer地址
+* [2] len: dma buffer 长度
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 void DMA_USART1_Rx_Init(u8 *dma_buf, s32 len)
 {
     NVIC_InitTypeDef NVIC_InitStructure;
     DMA_InitTypeDef DMA_InitStructure;
 
-    /* 1.使能DMA2时钟 */
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
 
-    /* 2.配置使用DMA接收数据 */
-    DMA_DeInit(DMA2_Stream2);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE); //使能DMA2时钟
+    DMA_DeInit(DMA2_Stream2); //配置使用DMA接收数据
 
-    DMA_InitStructure.DMA_Channel             = DMA_Channel_4;               	/* 配置DMA通道 */
-    DMA_InitStructure.DMA_PeripheralBaseAddr  = (uint32_t)(&(USART1->DR));   	/* 源 */
-    DMA_InitStructure.DMA_Memory0BaseAddr     = (uint32_t)dma_buf;             	/* 目的 */
-    DMA_InitStructure.DMA_DIR                 = DMA_DIR_PeripheralToMemory;    	/* 方向 */
-    DMA_InitStructure.DMA_BufferSize          = len;                    		/* 长度 */
-    DMA_InitStructure.DMA_PeripheralInc       = DMA_PeripheralInc_Disable;    	/* 外设地址是否自增 */
-    DMA_InitStructure.DMA_MemoryInc           = DMA_MemoryInc_Enable;         	/* 内存地址是否自增 */
-    DMA_InitStructure.DMA_PeripheralDataSize  = DMA_MemoryDataSize_Byte;      	/* 目的数据带宽 */
-    DMA_InitStructure.DMA_MemoryDataSize      = DMA_MemoryDataSize_Byte;      	/* 源数据宽度 */
-    DMA_InitStructure.DMA_Mode                = DMA_Mode_Circular;              	/* 单次传输模式/循环传输模式 */
-    DMA_InitStructure.DMA_Priority            = DMA_Priority_VeryHigh;        	/* DMA优先级 */
-    DMA_InitStructure.DMA_FIFOMode            = DMA_FIFOMode_Disable;          	/* FIFO模式/直接模式 */
-    DMA_InitStructure.DMA_FIFOThreshold       = DMA_FIFOThreshold_HalfFull; 	/* FIFO大小 */
-    DMA_InitStructure.DMA_MemoryBurst         = DMA_MemoryBurst_Single;       	/* 单次传输 */
+    DMA_InitStructure.DMA_Channel             = DMA_Channel_4;               	// 配置DMA通道
+    DMA_InitStructure.DMA_PeripheralBaseAddr  = (uint32_t)(&(USART1->DR));   	// 源
+    DMA_InitStructure.DMA_Memory0BaseAddr     = (uint32_t)dma_buf;             	// 目的
+    DMA_InitStructure.DMA_DIR                 = DMA_DIR_PeripheralToMemory;    	// 方向
+    DMA_InitStructure.DMA_BufferSize          = len;                    		// 长度
+    DMA_InitStructure.DMA_PeripheralInc       = DMA_PeripheralInc_Disable;    	// 外设地址是否自增
+    DMA_InitStructure.DMA_MemoryInc           = DMA_MemoryInc_Enable;         	// 内存地址是否自增
+    DMA_InitStructure.DMA_PeripheralDataSize  = DMA_MemoryDataSize_Byte;      	// 目的数据带宽
+    DMA_InitStructure.DMA_MemoryDataSize      = DMA_MemoryDataSize_Byte;      	// 源数据宽度
+    DMA_InitStructure.DMA_Mode                = DMA_Mode_Circular;              // 单次传输模式/循环传输模式
+    DMA_InitStructure.DMA_Priority            = DMA_Priority_VeryHigh;        	// DMA优先级
+    DMA_InitStructure.DMA_FIFOMode            = DMA_FIFOMode_Disable;          	// FIFO模式/直接模式
+    DMA_InitStructure.DMA_FIFOThreshold       = DMA_FIFOThreshold_HalfFull; 	// FIFO大小
+    DMA_InitStructure.DMA_MemoryBurst         = DMA_MemoryBurst_Single;       	// 单次传输
     DMA_InitStructure.DMA_PeripheralBurst     = DMA_PeripheralBurst_Single;
 
-    /* 3. 配置DMA */
-    DMA_Init(DMA2_Stream2, &DMA_InitStructure);
 
-    /* 4.由于接收不需要DMA中断，故不设置DMA中断 */
-    /* 4.使能DMA中断 */
-    DMA_ITConfig(DMA2_Stream2, DMA_IT_HT, ENABLE);
+    DMA_Init(DMA2_Stream2, &DMA_InitStructure); 	// 配置DMA
+    DMA_ITConfig(DMA2_Stream2, DMA_IT_HT, ENABLE); 	// 使能DMA中断
+    USART_DMACmd(USART1,USART_DMAReq_Rx,ENABLE);  	// 使能串口的DMA接收
 
-    /* 5.使能串口的DMA接收 */
-    USART_DMACmd(USART1,USART_DMAReq_Rx,ENABLE);
-
-    /* 6. 配置DMA中断优先级 */
+    // 配置DMA中断优先级
     NVIC_InitStructure.NVIC_IRQChannel                   = DMA2_Stream2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 2;
     NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
     NVIC_Init(&NVIC_InitStructure);
 
-    /* 7.使能DMA */
-    DMA_Cmd(DMA2_Stream2,ENABLE);
+    DMA_Cmd(DMA2_Stream2,ENABLE);   // 使能DMA
 }
 
+/********************************************************************************************************
+* 函数：void DMA2_Stream7_IRQHandler(void);
+* 描述:   USART1 DMA 发送中断处理
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 void DMA2_Stream7_IRQHandler(void)
 {
 	VOSIntEnter();
     if(DMA_GetITStatus(DMA2_Stream7,DMA_IT_TCIF7) != RESET)
     {
-        /* 清除标志位 */
+        // 清除标志位
         DMA_ClearFlag(DMA2_Stream7,DMA_IT_TCIF7);
-        /* 关闭DMA */
+        // 关闭DMA
         DMA_Cmd(DMA2_Stream7,DISABLE);
         /* 打开发送完成中断,确保最后一个字节发送成功 */
         //USART_ITConfig(USART1,USART_IT_TC,ENABLE);
@@ -196,6 +229,13 @@ void DMA2_Stream7_IRQHandler(void)
 	VOSIntExit ();
 }
 
+/********************************************************************************************************
+* 函数：s32 USART1_DMA_Send(u8 *data, s32 len);
+* 描述:   MA 发送
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 s32 USART1_DMA_Send(u8 *data, s32 len)
 {
 	s32 ret = 0;
@@ -205,17 +245,22 @@ s32 USART1_DMA_Send(u8 *data, s32 len)
     /* 等待空闲 */
 //    while (UART1_Use_DMA_Tx_Flag);
 //    UART1_Use_DMA_Tx_Flag = 1;
-    /* 复制数据 */
+    // 复制数据
     memcpy(SendBuff,data,len);
-    /* 设置传输数据长度 */
+    // 设置传输数据长度
     DMA_SetCurrDataCounter(DMA2_Stream7,len);
-    /* 打开DMA,开始发送 */
+    // 打开DMA,开始发送
     DMA_Cmd(DMA2_Stream7,ENABLE);
     return ret;
 }
 
-
-
+/********************************************************************************************************
+* 函数：void USART1_IRQHandler(void);
+* 描述:   USART1 接收中断处理， 接收线空闲状态监测中断
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 void USART1_IRQHandler(void)
 {
 	s32 len = 0;
@@ -226,10 +271,10 @@ void USART1_IRQHandler(void)
 	VOSIntEnter();
 
 #if USART_RX_USE_DMA
-    /* 接收完成中断 */
+    // 接收完成中断
     if(USART_GetITStatus(USART1, USART_IT_IDLE) != RESET)
     {
-        /* 清各种标志 */
+        // 清各种标志
     	data = USART1->SR;
     	data = USART1->DR;
 
@@ -240,9 +285,8 @@ void USART1_IRQHandler(void)
         len = RECV_BUF_SIZE - DMA_GetCurrDataCounter(DMA2_Stream2);
         if (gRingBuf && len > 0) {
         	ret = VOSRingBufSet(gRingBuf, RecvBuff, len);
-        	//kprintf("%d\r\n", ret);
         	if (ret != len) {
-        		kprintf("%s: ringbuf overflow!\r\n", __FUNCTION__);
+        		kprintf("warning: %s->VOSRingBufSet overflow!\r\n", __FUNCTION__);
         	}
         }
         //data_check(RecvBuff, len);
@@ -254,7 +298,7 @@ void USART1_IRQHandler(void)
         __local_irq_restore(irq_save);
     }
     else {
-    	/* 清各种标志 */
+    	// 清各种标志
     	data = USART1->SR;
         data = USART1->DR;
     }
@@ -280,6 +324,13 @@ void USART1_IRQHandler(void)
 	VOSIntExit ();
 } 
 
+/********************************************************************************************************
+* 函数：void DMA2_Stream2_IRQHandler(void);
+* 描述:   USART1 DMA 接收中断处理
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 void DMA2_Stream2_IRQHandler(void)
 {
 	s32 ret;
@@ -296,7 +347,7 @@ void DMA2_Stream2_IRQHandler(void)
         if (gRingBuf && len>0) {
         	ret = VOSRingBufSet(gRingBuf, RecvBuff, len);
         	if (ret != len) {
-        		kprintf("%s: ringbuf overflow!\r\n", __FUNCTION__);
+        		kprintf("warning: %s->VOSRingBufSet overflow!\r\n", __FUNCTION__);
         	}
         }
         DMA_SetCurrDataCounter(DMA2_Stream2,RECV_BUF_SIZE);
@@ -306,6 +357,13 @@ void DMA2_Stream2_IRQHandler(void)
 	VOSIntExit ();
 }
 
+/********************************************************************************************************
+* 函数：s32 vgetc(u8 *ch);
+* 描述:   任务通过vgetc获取一个字符
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 s32 vgetc(u8 *ch)
 {
 	s32 ret = 0;
@@ -319,6 +377,14 @@ s32 vgetc(u8 *ch)
 	return ret;
 
 }
+
+/********************************************************************************************************
+* 函数：s32 vgets(u8 *buf, s32 len);
+* 描述:   任务通过vget 获取一串字符
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 s32 vgets(u8 *buf, s32 len)
 {
 	s32 ret = 0;
@@ -331,6 +397,13 @@ s32 vgets(u8 *buf, s32 len)
 	return ret;
 }
 
+/********************************************************************************************************
+* 函数：s32 peek_vgets(u8 *buf, s32 len);
+* 描述:   任务通过peek_vgets窥视数据，但不取出
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 s32 peek_vgets(u8 *buf, s32 len)
 {
 	s32 ret = 0;
@@ -343,7 +416,13 @@ s32 peek_vgets(u8 *buf, s32 len)
 	return ret;
 }
 
-
+/********************************************************************************************************
+* 函数：void vputs(s8 *str, s32 len);
+* 描述:   任务通过vputs输出到串口
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 void vputs(s8 *str, s32 len)
 {
 	u32 irq_save;
@@ -357,6 +436,13 @@ void vputs(s8 *str, s32 len)
 	//__vos_irq_restore(irq_save);
 }
 
+/********************************************************************************************************
+* 函数：void dma_vputs(s8 *str, s32 len);
+* 描述:   MA 输出到串口
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
 void dma_vputs(s8 *str, s32 len)
 {
 	u32 irq_save;
@@ -367,7 +453,19 @@ void dma_vputs(s8 *str, s32 len)
 	__vos_irq_restore(irq_save);
 }
 
- 
 
-
+/********************************************************************************************************
+* 函数：int fputc(int ch, FILE *f);
+* 描述:   阻塞输出到串
+* 参数:   无
+* 返回：无
+* 注意：无
+*********************************************************************************************************/
+int fputc(int ch, FILE *f)
+{
+	USART1->SR = (uint16_t)~0x0040; //解决第一个字节发不出来问题
+	while((USART1->SR&0X40)==0);
+	USART1->DR = (u8) ch;
+	return ch;
+}
 
--- VOS_PRJ/usart/usart.h
+++ VOS_PRJ/usart/usart.h
@@ -5,6 +5,22 @@
 #include "stm32f4xx_conf.h"
 
 void uart_init(u32 bound);
+int fputc(int ch, FILE *f);
+void dma_vputs(s8 *str, s32 len);
+void vputs(s8 *str, s32 len);
+s32 peek_vgets(u8 *buf, s32 len);
+s32 vgets(u8 *buf, s32 len);
+s32 vgetc(u8 *ch);
+void DMA2_Stream2_IRQHandler(void);
+void USART1_IRQHandler(void);
+s32 USART1_DMA_Send(u8 *data, s32 len);
+void DMA2_Stream7_IRQHandler(void);
+void DMA_USART1_Rx_Init(u8 *dma_buf, s32 len);
+void DMA_USART1_Tx_Init(u8 *dma_buf, s32 len);
+
+void uart_init(u32 bound);
+void RegistUartEvent(s32 event, s32 task_id);
+
 
 #endif
 
